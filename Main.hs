2d0660f459dfbd4711c3b9eaac1dcf2cd1d3a7af
Dmitry Antonyuk <lomeo.nuke@gmail.com>
2010-06-18 10:17:38 +0400

    Initial commit.

---
+++ Main.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+import Data.List (delete, intersperse)
+import System.Console.CmdArgs
+import System.Environment (getEnvironment)
+
+data Options = Options  { filename :: String, number :: Int, line :: [String] }
+    deriving (Show, Data, Typeable)
+
+getDefaultFile = do
+    found <- fmap (lookup "HOME") getEnvironment
+    case found of
+        Nothing   -> error "There is no $HOME environment variable"
+        Just home -> return (home ++ "/.linecache")
+
+getOptions = do
+    defaultFile <- getDefaultFile
+    return $ mode Options
+        { filename = defaultFile &= typFile & text "Cache file path"
+        , number   = 10 &= typ "NUM" & text "Cache capacity"
+        , line     = def &= args &  typ "NEW-LINE" } &= text "Line cache manager"
+
+lru :: (Eq a) => a -> [a] -> [a]
+lru x xs = x : (x `delete` xs)
+
+force :: [a] -> IO ()
+force = mapM_ (\_ -> return ())
+
+main = do
+    options <- getOptions
+    Options filename number rest <- cmdArgs "linecache v 0.1, (C) Dmitry Antonyuk 2010" [options]
+
+    cache <- fmap lines $ readFile filename `catch` (\_ -> return "")
+
+    let handle ""   = list
+        handle line = add
+
+        list = putStr (unlines cache)
+
+        add = do
+            let newCache = take number $ lru line cache
+            force newCache
+            writeFile filename (unlines newCache)
+
+    handle $ concat $ intersperse " " rest
+  where
+
